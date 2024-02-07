0214b0fca3d0f4cd436a81fe65cb99f7e5e1d5e0
frasergmasson <frasergmasson@gmail.com>
2024-02-07 14:23:51 +0000

    Check LRC value

--- src/sts/test/validator/CompletedValidator.java
+++ src/sts/test/validator/CompletedValidator.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 public class CompletedValidator implements Validator{
 
     final Pattern pattern;
+    final byte DLE = 0x10;
 
     private String convertBytesToString(byte[] message){
         String[] stringBytes = new String[message.length];
@@ -22,11 +23,21 @@ public class CompletedValidator implements Validator{
         return matcher.matches(); 
     }
 
+    private boolean checkLRC(byte[] message){
+        byte calculatedLRC = 0;
+        for(int i=1;i<message.length-1;i++){ //Start and end are set to disclude STX and LRC
+            if (message[i] != DLE || message[i-1] == DLE){ // XOR is not performed if the value is DLE
+                calculatedLRC ^= message[i];
+            }
+        }  
+        return calculatedLRC==message[message.length-1];
+    } 
+
 
     @Override
     public boolean isValid(byte[] message){
         String stringMessage = convertBytesToString(message);
-        return checkFormat(stringMessage);    
+        return checkFormat(stringMessage) && checkLRC(message); 
     }
 
     public CompletedValidator(){
@@ -34,7 +45,7 @@ public class CompletedValidator implements Validator{
             "0x02" // STX
             + "(" // Data
                 + "0x100x(02|03|10)" //0x10 followed by a byte that needs to be escaped
-                + "|(0x((0[^23])|1[^0]))" //Any byte that does not need to be escaped (not 0x02,0x03,or0x10)
+                + "|(0x(0[^23]|(1[^0])|[^01].))" //Any byte that does not need to be escaped (not 0x02,0x03,or0x10)
             + ")*"// Zero or more bytes
             + "0x03" // ETX
             + "0x.."; // LRC
