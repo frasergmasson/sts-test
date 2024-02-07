e30dfeece608053966a3907de8861d75a9fb79fc
frasergmasson <frasergmasson@gmail.com>
2024-02-07 14:29:47 +0000

    Remove unecessary '0x' from regex

--- src/sts/test/validator/CompletedValidator.java
+++ src/sts/test/validator/CompletedValidator.java
@@ -7,12 +7,11 @@ import java.util.regex.Pattern;
 public class CompletedValidator implements Validator{
 
     final Pattern pattern;
-    final byte DLE = 0x10;
 
     private String convertBytesToString(byte[] message){
         String[] stringBytes = new String[message.length];
         for(int i=0;i<message.length;i++){
-            stringBytes[i] = String.format("0x%02X", message[i]);
+            stringBytes[i] = String.format("%02X", message[i]);
         }
         return String.join("",stringBytes);
     }
@@ -26,7 +25,7 @@ public class CompletedValidator implements Validator{
     private boolean checkLRC(byte[] message){
         byte calculatedLRC = 0;
         for(int i=1;i<message.length-1;i++){ //Start and end are set to disclude STX and LRC
-            if (message[i] != DLE || message[i-1] == DLE){ // XOR is not performed if the value is DLE
+            if (message[i] != 0x10 || message[i-1] == 0x10){ // XOR is not performed if the value is a DLE
                 calculatedLRC ^= message[i];
             }
         }  
@@ -36,19 +35,19 @@ public class CompletedValidator implements Validator{
 
     @Override
     public boolean isValid(byte[] message){
-        String stringMessage = convertBytesToString(message);
-        return checkFormat(stringMessage) && checkLRC(message); 
+        return checkFormat(convertBytesToString(message)) 
+            && checkLRC(message); 
     }
 
     public CompletedValidator(){
         String regEx = 
-            "0x02" // STX
+            "02" // STX
             + "(" // Data
-                + "0x100x(02|03|10)" //0x10 followed by a byte that needs to be escaped
-                + "|(0x(0[^23]|(1[^0])|[^01].))" //Any byte that does not need to be escaped (not 0x02,0x03,or0x10)
+                + "10(02|03|10)" //0x10 followed by a byte that needs to be escaped
+                + "|((0[^23]|(1[^0])|[^01].))" //Any byte that does not need to be escaped (not 0x02,0x03,or0x10)
             + ")*"// Zero or more bytes
-            + "0x03" // ETX
-            + "0x.."; // LRC
+            + "03" // ETX
+            + ".."; // LRC
 
         pattern = Pattern.compile(regEx);
     }
